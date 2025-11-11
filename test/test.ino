5415036b4397de321eb7384957ffe3569ecab680
Lucas Emanuel Sabino de Souza Lima <lessl@windows.cin.ufpe.br>
2025-11-11 11:16:54 -0300

    feat: add power down between temperature readings

--- test/test.ino
+++ test/test.ino
@@ -1,4 +1,7 @@
 #include <Temperature.h>
+#include <Sleep.h>
+
+#define SLEEP_TIME 10
 
 TemperatureSensor sensor(2);
 
@@ -25,4 +28,10 @@ void loop() {
   else {
     Serial.println("Sensor disconnected");
   }
+
+  // Esvazia o buffer serial antes de ir dormir
+  Serial.flush();
+
+  // Entra em modo power down
+  Sleep::sleep(SLEEP_TIME);
 }
