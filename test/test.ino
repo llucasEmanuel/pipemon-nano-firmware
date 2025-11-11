47c52bcc0ecd514f7ed954a66f3b00595f4b5b7c
Lucas Emanuel Sabino de Souza Lima <lessl@windows.cin.ufpe.br>
2025-11-11 08:06:28 -0300

    feat: add LED indicator for high temperature

--- test/test.ino
+++ test/test.ino
@@ -5,14 +5,22 @@ TemperatureSensor sensor(2);
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
   sensor.init();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   float temp = sensor.getTemperature();
+
   if (sensor.isConnected()) {
     Serial.println(temp);
+    if (temp > 27) {
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
+    else {
+      digitalWrite(LED_BUILTIN, LOW);
+    }
   }
   else {
     Serial.println("Sensor disconnected");
